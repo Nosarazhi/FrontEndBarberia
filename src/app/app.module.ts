72869aec700803a1cb2c0960f013c5c29054f9a0
Lean <nosarazhi@gmail.com>
2023-10-03 14:10:07 -0300

    Tercer Cambia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ServicioComponent } from './servicio/servicio.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { ServiceListComponent } from './lista-servicios/lista-servicios.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ServicioComponent,
-    ContactoComponent,
-    ServiceListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/servicio/servicio.component.ts
+++ src/app/servicio/servicio.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./servicio.component.css']
 })
 export class ServicioComponent {
-
+  servicios = [
+    {
+      nombre: 'Corte de Pelo',
+      imagen: 'url-de-la-imagen',
+      descripcion: 'Descripción del servicio'
+    },
+    {
+      nombre: 'Limpieza facial',
+      imagen: 'assets/limpiezafacial.jfif',
+      descripcion: 'Nuestra limpieza facial es más que un simple tratamiento de belleza. Es una experiencia de bienestar integral que no solo mejora la apariencia de tu piel, sino que también te proporciona un momento de relajación y cuidado personal. Utilizamos productos de alta calidad que son suaves con tu piel y la dejan con una sensación de frescura y renovación. Este servicio es perfecto para prepararte para un evento especial o simplemente para darte un capricho. Ven y descubre la diferencia que puede hacer una limpieza facial profesional en tu rutina de belleza.'
+    },
+    {
+      nombre: 'Teñido de Cabello',
+      imagen: 'url-de-la-imagen',
+      descripcion: 'Descripción del servicio'
+    },
+    {
+      nombre: 'Depilación Corporal',
+      imagen: 'url-de-la-imagen',
+      descripcion: 'Descripción del servicio'
+    },
+    {
+      nombre: 'Masajes',
+      imagen: 'url-de-la-imagen',
+      descripcion: 'Descripción del servicio'
+    },
+  ];
 }
