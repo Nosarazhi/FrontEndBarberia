c5eca7fac0800cc0936c86ded79bf31ad5b92940
Lean <nosarazhi@gmail.com>
2023-10-16 08:35:49 -0300

    Quinto Cambio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  servicios: any[] = [];
 
+  constructor() {
+    this.servicios = [
+      {
+        nombre: 'Corte de Pelo',
+        imagen: 'assets/CorteDePelo.png',
+      },
+      {
+        nombre: 'Limpieza facial',
+        imagen: 'assets/LimpiezaFacial.jpg',
+      },
+      {
+        nombre: 'Teñido de Cabello',
+        imagen: 'assets/TeñidoCabello.jpg',
+      },
+      {
+        nombre: 'Depilación Facial',
+        imagen: 'assets/DepilacionFacial.jpg',
+      },
+      {
+        nombre: 'Masajes',
+        imagen: 'assets/MasajeFacial.jpg',
+      }
+    ];
+  }
 }
